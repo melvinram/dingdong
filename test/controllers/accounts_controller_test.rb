5433de367bf73688f998987650e9b1ca854a483b
Melvin Ram <mr@melvinram.com>
2018-09-05 16:56:31 -0700

    Add basic AccountsController test. Failing.

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class AccountsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create account" do
+    post "/accounts", params: {
+      account: {
+        name: 'Melvin Ram',
+        subdomain: 'mel',
+        owner: {
+          email: 'mel@example.com',
+          password: '123456'
+        }
+      }
+    }
+    
+    account = Account.where(subdomain: 'mel').first
+    assert account, 'account not created'
+  end
 end
