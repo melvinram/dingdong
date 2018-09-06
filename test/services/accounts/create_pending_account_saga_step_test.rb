de7cae46e5427bf4fca4f13ae3e32a8d8605990d
Melvin Ram <mr@melvinram.com>
2018-09-05 21:02:53 -0700

    Nest class inside module instead of inline

--- test/services/accounts/create_pending_account_saga_step_test.rb
+++ test/services/accounts/create_pending_account_saga_step_test.rb
@@ -1,23 +1,26 @@
 require 'test_helper'
 
-class Accounts::CreatePendingAccountSagaStepTest < ActiveSupport::TestCase
-  test 'runs steps' do
-    valid_account_params = { account: { name: 'Mel', subdomain: 'mel' } }
-    outcome_data = {}
-    
-    Accounts::CreatePendingAccountSagaStep.new(
-      params: valid_account_params,
-      outcome_data: outcome_data
-    ).step_forward
+module Accounts
+  class CreatePendingAccountSagaStepTest < ActiveSupport::TestCase
+    test '#step_forward creates pending account with valid params' do
+      valid_account_params = { account: { name: 'Mel', subdomain: 'mel' } }
+      outcome_data = {}
 
-    account_id = outcome_data[:account_id]
-    assert account_id, 'account id for new account not found in outcome data'
+      Accounts::CreatePendingAccountSagaStep.new(
+        params: valid_account_params,
+        outcome_data: outcome_data
+      ).step_forward
 
-    account = ::Account.find(outcome_data[:account_id])
-    assert account, 'account not found for account id collected'
+      account_id = outcome_data[:account_id]
+      assert account_id, 'account id for new account not found in outcome data'
 
-    assert_equal 'mel', account.subdomain
-    assert account.pending?, 'new account not in pending state'
-    refute account.active?, 'new account is in active state'
+      account = ::Account.find(outcome_data[:account_id])
+      assert account, 'account not found for account id collected'
+
+      assert_equal 'mel', account.subdomain
+      assert account.pending?, 'new account not in pending state'
+      refute account.active?, 'new account is in active state'
+    end
+
   end
 end
